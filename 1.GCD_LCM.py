f674b8c76269e3184221f8c5328bf7fe50d57c2b
joeyguo2013 <joeyguo2013@gmail.com>
2025-12-30 14:05:45 +0800

    Add files via upload

---
+++ 1.GCD_LCM.py
@@ -0,0 +1,28 @@
+a = int(input('please input number A'))
+b = int(input('please input number B'))
+common_divisor_list = [0] * a
+common_divisor = 0
+common_multiple = 0
+number = 0
+if a > b:
+    a, b = b, a
+for i in range(1, a + 1):
+    if a % i == 0 and b % i == 0 and i <= a:
+        common_divisor_list[number] = int(i)
+        number += 1
+number = 0
+while True:
+    if common_divisor_list[number] > common_divisor:
+        common_divisor = common_divisor_list
+        number += 1
+    else:
+        break
+number = 0
+while common_multiple == 0:
+    if number % a == 0 and number % b == 0 and number != 0:
+        common_multiple = number
+        break
+    else:
+        number += 1
+print(f'The greatest common divisor is{common_divisor}')
+print(f'The least common multiple is{common_multiple}')
