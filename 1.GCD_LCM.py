b6628c2e5617dfa1ca5b732d5d4f2ad83c077238
Ross <qtguo@qq.com>
2025-12-30 17:24:59 +0800

    modify

--- 1.GCD_LCM.py
+++ 1.GCD_LCM.py
@@ -1,5 +1,17 @@
-a = int(input('please input number A'))
-b = int(input('please input number B'))
+while True:
+    a = input('please input number A ')
+    if a == 0 or not a.isdigit():
+        print("please don't input character and 0")
+    else:
+        break
+a = int(a)
+while True:
+    b = input('please input number B ')
+    if b == 0 or not b.isdigit():
+        print("please don't input character and 0")
+    else:
+        break
+b = int(b)
 common_divisor_list = [0] * a
 common_divisor = 0
 common_multiple = 0
@@ -7,7 +19,7 @@ number = 0
 if a > b:
     a, b = b, a
 for i in range(1, a + 1):
-    if a % i == 0 and b % i == 0 and i <= a:
+    if a % i == 0 and b % i == 0:
         common_divisor_list[number] = int(i)
         number += 1
 number = 0
@@ -17,12 +29,11 @@ while True:
         number += 1
     else:
         break
-number = 0
+number = 1
 while common_multiple == 0:
-    if number % a == 0 and number % b == 0 and number != 0:
+    if number % a == 0 and number % b == 0:
         common_multiple = number
-        break
     else:
         number += 1
-print(f'The greatest common divisor is{common_divisor}')
-print(f'The least common multiple is{common_multiple}')
+print(f'The greatest common divisor is {common_divisor}')
+print(f'The least common multiple is {common_multiple}')
