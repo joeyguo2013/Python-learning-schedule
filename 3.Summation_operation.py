5c5259c0787e3a668bec4d8bc3f9511de35813d6
joeyguo2013 <GitHub>
2026-01-02 12:07:03 +0800

    Optimize code logic

--- 3.Summation_operation.py
+++ 3.Summation_operation.py
@@ -1,23 +1,23 @@
 #input
+
 while True:
-    while True:
-        a = input('What do you want to find the sum of?')
-        if not a.isdigit() or int(a) == 0:
-            print("please don't input character and 0")
-        else:
-            break
-    a = int(a)
-    while True:
-        b = input('How many numbers do you want to add?')
-        if not b.isdigit() or int(b) == 0 :
-            print("please don't input character and 0")
-        else:
-            break
-    b = int(b)
-    if b > 10000:
+    a = input('What do you want to find the sum of? ')
+    if not a.isdigit() or int(a) == 0:
+        print("please don't input character and 0")
+    elif int(a) >= 10:
+        print('your input cannot bigger than 10')
+    else:
+        break
+a = int(a)
+while True:
+    b = input('How many numbers do you want to add? ')
+    if not b.isdigit() or int(b) == 0 :
+        print("please don't input character and 0")
+    elif int(b) > 10000:
         print('your computer cannot do this!!!!it was so big!!!')
     else:
         break
+b = int(b)
 pure_process = [0] * b
 number = 0
 for i in range(0, b):
---
+++ 4.sum_of_factorial.py
@@ -0,0 +1,15 @@
+while True:
+    number = input('What factorial do you want to multiply to?')
+    if not number.isdigit() or int(number) == 0 :
+        print("please don't input character and 0")
+    elif int(number) > 30:
+        print('your computer cannot do this!!!!it was so big!!!')
+    else:
+        break
+number = int(number)
+final_number = 0
+process = 1
+for i in range(1, number + 1):
+    process = process * i
+    final_number = process + final_number
+print(f'The final number is {final_number}')
