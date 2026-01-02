9219ea1d31a21257333dba54114b32e72c4f2d7d
joeyguo2013 <GitHub>
2026-01-02 12:28:05 +0800

    Resolve merge conflict and update code

--- 3.Summation_operation.py
+++ 3.Summation_operation.py
@@ -1,28 +1,31 @@
-#input
+# Input processing
 while True:
-    while True:
-        a = input('What do you want to find the sum of?')
-        if not a.isdigit() or int(a) == 0:
-            print("please don't input character and 0")
-        else:
-            break
-    a = int(a)
-    while True:
-        b = input('How many numbers do you want to add?')
-        if not b.isdigit() or int(b) == 0 :
-            print("please don't input character and 0")
-        else:
-            break
-    b = int(b)
-    if b > 10000:
-        print('your computer cannot do this!!!!it was so big!!!')
+    a = input('What do you want to find the sum of? ')
+    if not a.isdigit() or int(a) == 0:
+        print("Please don't input characters or 0")
+    elif int(a) >= 10:
+        print('Your input cannot be bigger than 10')
     else:
         break
+a = int(a)
+
+while True:
+    b = input('How many numbers do you want to add? ')
+    if not b.isdigit() or int(b) == 0:
+        print("Please don't input characters or 0")
+    elif int(b) > 10000:
+        print('Your computer cannot handle this! It is too big!')
+    else:
+        break
+b = int(b)
+
+# Generate each number
 pure_process = [0] * b
 number = 0
-for i in range(0, b):
+for i in range(b):
     number = number * 10 + a
     print(number)
     pure_process[i] = number
-#print
-print(f'all number sum is {sum(pure_process)}')
+
+# Print the sum
+print(f'The sum of all numbers is {sum(pure_process)}')
